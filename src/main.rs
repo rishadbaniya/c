cb3aceb4e5143ad6061328cf0775c34c16da47f6
rishadbaniya <rbdroid01@gmail.com>
2022-01-11 11:54:11 +0530

    get path from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Gets all the arguments and skips the first one(which is the relative path of the binary from where
+    // the command is called)
+    let arguments: Vec<String> = env::args().skip(1).collect();
+
+    // Source and destination path
+    let paths = (PathBuf::from(&arguments[0]), PathBuf::from(&arguments[1]));
 }
