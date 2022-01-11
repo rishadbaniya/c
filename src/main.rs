34cdc77211d24b8cab0b7cb1fe42cfdd32fa434c
rishadbaniya <rbdroid01@gmail.com>
2022-01-11 12:21:14 +0530

    add copy call from std

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use std::fs;
+
 fn main() {
     // Gets all the arguments and skips the first one(which is the relative path of the binary from where
     // the command is called)
@@ -10,4 +12,7 @@ fn main() {
 
     // Source and destination path
     let paths = (PathBuf::from(&arguments[0]), PathBuf::from(&arguments[1]));
+
+    // Copies a regular file from source to destination
+    fs::copy(paths.0, paths.1);
 }
